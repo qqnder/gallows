e56b603215bf5e8d1f6f515ecb4bfdcc39cf56cd
qqnder <ruseverus@gmail.com>
2021-02-17 20:25:25 +0300

    Initial Commit

---
+++ gallows/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gallows
+//
+//  Created by Kiwi on 17.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
